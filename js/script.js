ed80a1f50128fa71466d3b4c470227f158a17dff
IcyKitMac <icykitdesign@gmail.com>
2022-04-10 22:35:53 +0300

    Hours variable fixed

--- js/script.js
+++ js/script.js
@@ -1,35 +1,35 @@
 const deadline = '2022-04-18'; // My birthday btw ^_^
 
 function getTimeRemaining(endtime) {
-        const t = Date.parse(endtime) - Date.parse(new Date()),
-              days = Math.floor(t / (1000 * 60 * 60 * 24)),
-              hours = Math.floor((t / 1000 * 60 * 60) % 24),
-              minutes = Math.floor((t / 1000 / 60) % 60),
-              seconds = Math.floor((t / 1000) % 60);
-        return {
+    const t = Date.parse(endtime) - Date.parse(new Date()),
+        days = Math.floor(t / (1000 * 60 * 60 * 24)),
+        hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+        minutes = Math.floor((t / 1000 / 60) % 60),
+        seconds = Math.floor((t / 1000) % 60);
+    return {
         'total': t,
         'days': days,
         'hours': hours,
         'minutes': minutes,
         'seconds': seconds,
-        };
+    };
 }
 
 function getZero(num) {
-    if(num >= 0 && num < 10) {
+    if (num >= 0 && num < 10) {
         return `0${num}`;
     } else {
         return num;
-    } 
+    }
 } // We add zero when the number is less than 10. It looks more beautiful this way
 
 function setClock(parentSelector, endtime) {
     const timer = document.querySelector(parentSelector),
-          days = timer.querySelector('#days'),
-          hours = timer.querySelector('#hours'),
-          minutes = timer.querySelector('#minutes'),
-          seconds = timer.querySelector('#seconds'),
-          timeInterval = setInterval(reloadTimer, 1000); // Updating of timer
+        days = timer.querySelector('#days'),
+        hours = timer.querySelector('#hours'),
+        minutes = timer.querySelector('#minutes'),
+        seconds = timer.querySelector('#seconds'),
+        timeInterval = setInterval(reloadTimer, 1000); // Updating of timer
 
     reloadTimer();
 
